e2917fd63f5701365a024bfc27c8db8265bd903b
Moveon <moveon_0827@outlook.com>
2024-09-02 16:46:14 +0800

    init

---
+++ src/main/java/cool/moveon/lmt/LocalMessageTableTransactionApplication.java
@@ -0,0 +1,13 @@
+package cool.moveon.lmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalMessageTableTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalMessageTableTransactionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cool/moveon/lmt/LocalMessageTableTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package cool.moveon.lmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalMessageTableTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
